335495780794eabb34da11f93bf2e8911631263b
Bipeen Sinha <bipeen.sinha@hotmail.com>
2024-02-28 12:02:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name      = example
-  location  = "east us"
+  name      = var.resource_group_name
+  location  = var.location
 }
 
 # Network
