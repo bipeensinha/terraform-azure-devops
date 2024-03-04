d1ce13991260fbff5a5acc8748432f0d21895a00
Bipeen Sinha <bipeen.sinha@hotmail.com>
2024-03-04 12:41:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
       backend "azurerm" {
+        resource_group_name  = "az800"
         storage_account_name  = "tfdemo19752004"
         container_name        = "tfstate"
         key                   = "terraform.tfstate"
