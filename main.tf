c3a8b0b8bee2f21de35b3fe76ba71e43a599811a
Bipeen Sinha <bipeen.sinha@hotmail.com>
2024-03-04 13:12:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,15 @@ terraform {
     }
   }
 }
+variable "default_tags" {
+  description = "The defaults tags, we will be adding to the these"
+  type        = map
+  default = {
+    project        = "devops-pipeline-test"
+    environment    = "dev"
+    deployed_using = "terraform"
+  }
+}
 terraform {
       backend "azurerm" {
         resource_group_name  = "az800"
